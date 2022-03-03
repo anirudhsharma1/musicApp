17f27e23f09728a041436966286a31561c963a85
anirudhsharma1 <anirudhsharma885@outlook.com>
2022-03-03 23:36:53 +0530

    Project initialized

---
+++ src/components/MusicCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MusicCard.css'
+
+const MusicCard = () => {
+  return (
+    <div className="container">
+    <div className="music-content">
+        <h3>PLAYER</h3>
+        </div>
+        </div>
+  )
+}
+
+export default MusicCard
